f35b81e95424fdbc58af430ad28570962c3ccaee
durgadasdeshmukh <ddurgadas83@gmail.com>
2025-09-20 17:47:30 +0530

    My first code commit in to the git "Hello World"

---
+++ src/ex_001_learningJava/Lab001_Helloworld.java
@@ -0,0 +1,7 @@
+package ex_001_learningJava;
+
+public class Lab001_Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java World Program Durgadas Deshmukh");
+    }
+}
